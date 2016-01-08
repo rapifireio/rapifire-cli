49368248f9bfaa722d1e9331ccdc504ee44551f5
prozz <pawel.rozynek@sentaca.com>
2016-01-08 13:19:32 +0100

    base ws url in config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  baseWebUrl: "http://rapifire.com/api/web/v1",
-  baseApiUrl: "http://api.rapifire.com/api/v1"
+    baseWebUrl: "http://rapifire.com/api/web/v1",
+    baseApiUrl: "http://api.rapifire.com/api/v1",
+    baseWsUrl: "ws://ws.rapifire.com/pubsub"
 };
-
