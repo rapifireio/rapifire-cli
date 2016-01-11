60ce95f239a3959355a0efa9d71553d2343ed181
Pawel Jablonski <pawel.jablonski@sentaca.com>
2016-01-11 16:15:57 +0100

    searching products,users,things by name - moved common code to tools

--- tools.js
+++ tools.js
@@ -1,6 +1,12 @@
 var fs = require("fs");
 var path = require("path");
 var WebSocket = require('ws');
+var rest = require('restler');
+var config = require('./config');
+
+var getProductByNameUrl = config.baseApiUrl + "/products/?name=";
+var getUserByNameUrl = config.baseApiUrl + "/users/?username=";
+var getThingByNameUrl = config.baseApiUrl + "/things/"
 
 module.exports = {
 	getProfileFilename : function() {
@@ -63,5 +69,40 @@ module.exports = {
             console.log("-> " + JSON.stringify(packet));
             ws.send(JSON.stringify(packet));
         });
-    }
+    },
+    doForProductName: function(productName, callback) {
+	  rest.get(getProductByNameUrl + productName, this.getAuthObj())
+	      .on('success', function(data, response) {
+	        if(data.length != 1){
+	          process.exit(1);
+	        }
+	        var productId = data[0].id;
+	        callback(productId);
+	      }).on('fail', this.failureHandler);
+	},
+	doForUserName: function(username, callback) {
+	  rest.get(getUserByNameUrl + username, this.getAuthObj())
+	    .on('success', function(data, response) {
+	      if(data.length != 1){
+	        process.exit(1);
+	      }
+	      var userId = data[0].id;
+	      callback(userId);
+	    }).on('fail', this.failureHandler);
+	},
+	doForThingName: function(thingName, productName, callback) {
+	  var auth = this.getAuthObj();
+	  var failureHandler = this.failureHandler
+	  this.doForProductName(productName, function(productId) {
+	    rest.get(getThingByNameUrl + "?productId=" + productId + "&thingName=" + thingName, auth)
+	      .on('success', function(data, response){
+	        if(data.length != 1){
+	          process.exit(1);
+	        }
+	        var thingId = data[0].thingId;
+	        callback(thingId, productId);
+	      })
+	      .on('fail', failureHandler);
+	  });
+	}
 }
