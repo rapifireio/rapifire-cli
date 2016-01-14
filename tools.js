44f609cee59df670e5ef6e70773062def92f0430
Pawel Jablonski <pawel.jablonski@sentaca.com>
2016-01-14 09:16:16 +0100

    encode uri params

--- tools.js
+++ tools.js
@@ -9,96 +9,99 @@ var getUserByNameUrl = config.baseApiUrl + "/users/?username=";
 var getThingByNameUrl = config.baseApiUrl + "/things/"
 
 module.exports = {
-    getProfileFilename : function() {
-	return (process.env.HOME || process.env.USERPROFILE) + path.sep + ".rapifire-cli";
-    },
-    readConfig : function() {
-	var buf = fs.readFileSync(this.getProfileFilename());
-	return JSON.parse(buf);
-    },
-    writeConfig : function(obj) {
-	fs.writeFileSync(this.getProfileFilename(), JSON.stringify(obj));
-    },
-    failureHandler : function(data, response) {
-	console.log("REST call failed. Status code:", response.statusCode, "Body:", data);
-    },
-    printJson : function(json){
-	// last parameter is number of spaces
-	console.log(JSON.stringify(json, null, 4));
-    },
-    printJsonArray : function(jsonArray) {
-	for(var key in jsonArray) {
-	    this.printJson(jsonArray[key]);
-	}
-    },
-    getAuthObj : function() {
-	var config = this.readConfig();
-	return {username: config.username, password: config.password};
-    },
-    subscribe: function(authId, authKey, channel, url) {
-        var init = {
-            "operation": "init",
-            "data": {
-                "authId": authId,
-                "authKey": authKey,
-            }
-        };
-
-        var packet = {
-            "operation": "subscribe",
-            "data": {
-                "channel": channel
-            }
-        };
-
-        var ws = new WebSocket(url);
-
-        ws.on('message', function(data) {
-            console.log(data);
-        });
-
-        ws.on('close', function close() {
-            process.exit(0);
-            ws = null;
-        });
-
-        ws.on('open', function() {
-            ws.send(JSON.stringify(init));
-            ws.send(JSON.stringify(packet));
-        });
-    },
-    doForProductName: function(productName, callback) {
-	rest.get(getProductByNameUrl + productName, this.getAuthObj())
-	    .on('success', function(data, response) {
-	        if(data.length != 1){
-                    console.log("Found " + data.length + " products. Expected single product.");
-	            process.exit(1);
-	        }
-	        var productId = data[0].id;
-	        callback(productId);
-	    }).on('fail', this.failureHandler);
-	},
-    doForUserName: function(username, callback) {
-	rest.get(getUserByNameUrl + username, this.getAuthObj())
-	    .on('success', function(data, response) {
-	        if(data.length != 1){
-                    console.log("Found " + data.length + " users. Expected single user.");
-	            process.exit(1);
-	        }
-	        var userId = data[0].id;
-	        callback(userId);
-	    }).on('fail', this.failureHandler);
-    },
-    doForThingName: function(thingName, callback) {
-	rest.get(getThingByNameUrl + "?thingName=" + thingName, this.getAuthObj())
-	    .on('success', function(data, response){
-		if(data.length != 1){
-                    console.log("Found " + data.length + " things. Expected single thing.");
-		    process.exit(1);
-		}
-		var thingId = data[0].thingId;
-	        callback(thingId);
-	    })
-	    .on('fail', this.failureHandler);
+  getProfileFilename: function() {
+    return (process.env.HOME || process.env.USERPROFILE) + path.sep + ".rapifire-cli";
+  },
+  readConfig: function() {
+    var buf = fs.readFileSync(this.getProfileFilename());
+    return JSON.parse(buf);
+  },
+  writeConfig: function(obj) {
+    fs.writeFileSync(this.getProfileFilename(), JSON.stringify(obj));
+  },
+  failureHandler: function(data, response) {
+    console.log("REST call failed. Status code:", response.statusCode, "Body:", data);
+  },
+  printJson: function(json) {
+    // last parameter is number of spaces
+    console.log(JSON.stringify(json, null, 4));
+  },
+  printJsonArray: function(jsonArray) {
+    for (var key in jsonArray) {
+      this.printJson(jsonArray[key]);
     }
+  },
+  getAuthObj: function() {
+    var config = this.readConfig();
+    return {
+      username: config.username,
+      password: config.password
+    };
+  },
+  subscribe: function(authId, authKey, channel, url) {
+    var init = {
+      "operation": "init",
+      "data": {
+        "authId": authId,
+        "authKey": authKey,
+      }
+    };
+
+    var packet = {
+      "operation": "subscribe",
+      "data": {
+        "channel": channel
+      }
+    };
+
+    var ws = new WebSocket(url);
+
+    ws.on('message', function(data) {
+      console.log(data);
+    });
+
+    ws.on('close', function close() {
+      process.exit(0);
+      ws = null;
+    });
+
+    ws.on('open', function() {
+      ws.send(JSON.stringify(init));
+      ws.send(JSON.stringify(packet));
+    });
+  },
+  doForProductName: function(productName, callback) {
+    rest.get(getProductByNameUrl + encodeURIComponent(productName), this.getAuthObj())
+      .on('success', function(data, response) {
+        if (data.length != 1) {
+          console.log("Found " + data.length + " products. Expected single product.");
+          process.exit(1);
+        }
+        var productId = data[0].id;
+        callback(productId);
+      }).on('fail', this.failureHandler);
+  },
+  doForUserName: function(username, callback) {
+    rest.get(getUserByNameUrl + encodeURIComponent(username), this.getAuthObj())
+      .on('success', function(data, response) {
+        if (data.length != 1) {
+          console.log("Found " + data.length + " users. Expected single user.");
+          process.exit(1);
+        }
+        var userId = data[0].id;
+        callback(userId);
+      }).on('fail', this.failureHandler);
+  },
+  doForThingName: function(thingName, callback) {
+    rest.get(getThingByNameUrl + "?thingName=" + encodeURIComponent(thingName), this.getAuthObj())
+      .on('success', function(data, response) {
+        if (data.length != 1) {
+          console.log("Found " + data.length + " things. Expected single thing.");
+          process.exit(1);
+        }
+        var thingId = data[0].thingId;
+        callback(thingId);
+      })
+      .on('fail', this.failureHandler);
+  }
 }
